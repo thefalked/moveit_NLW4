35623ec7fb32f92eed0aab89f5d1e88ffc39335e
Giuliano Crivelli <giuliano@agenciaw3.digital>
2021-02-24 09:14:00 -0400

    added profile, completed challenges and started countdown

---
+++ src/component/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/thefalked.png" alt="Giu"/>
+      <div>
+        <strong>
+          Giuliano
+        </strong>
+        <p>
+          <img src="icons/level.svg" alt="level"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,33 @@
+import { CompletedChallenges } from '../component/CompletedChallenges';
+import { Countdown } from '../component/Countdown';
+import { ExperienceBar } from '../component/ExperienceBar';
+import { Profile } from '../component/Profile';
+
 import Head from 'next/head';
 
-import { ExperienceBar } from '../component/ExperienceBar';
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>
+          Início | Move.it
+        </title>
+      </Head>
+
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
